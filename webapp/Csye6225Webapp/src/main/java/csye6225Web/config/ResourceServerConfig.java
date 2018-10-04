52219e74b71e9c83e818a08cdeb561bf5720471d
leo <cheng.li@husky.neu.edu>
2018-10-04 17:17:57 -0400

    commitedChange

--- webapp/Csye6225Webapp/src/main/java/csye6225Web/config/ResourceServerConfig.java
+++ webapp/Csye6225Webapp/src/main/java/csye6225Web/config/ResourceServerConfig.java
@@ -5,10 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-/**
- *The @EnableResourceServer annotation adds a filter of type OAuth2AuthenticationProcessingFilter automatically
- *to the Spring Security filter chain.
- */
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
