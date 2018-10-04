66739f49317abd0675fa85552a301d55056c9a8c
howson <howson@localhost.localdomain>
2018-10-04 17:05:21 -0400

    new

---
+++ web-z/src/main/java/com/github/arocketman/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.github.arocketman.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public String index(){
+        return "Hello world";
+    }
+
+    @GetMapping(value = "/private")
+    public String privateArea(){
+        return "Private area";
+    }
+
+    @GetMapping(value = "/private/p")
+    public String privatArea(){
+        return "Private area";
+    }
+
+}
