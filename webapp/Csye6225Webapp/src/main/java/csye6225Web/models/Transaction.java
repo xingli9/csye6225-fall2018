ccdc9e308a3b51a0ac8412206a0c68130755f93b
howson <howson@localhost.localdomain>
2018-10-04 20:28:14 -0400

    update

--- webapp/Csye6225Webapp/src/main/java/csye6225Web/models/Transaction.java
+++ webapp/Csye6225Webapp/src/main/java/csye6225Web/models/Transaction.java
@@ -52,36 +52,36 @@ public class Transaction {
         return description;
     }
 
-
     public String getMerchant() {
         return merchant;
     }
 
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
     }
 
     public void setDate(String date) {
         this.date = date;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
     }
 
     public void setMerchant(String merchant) {
         this.merchant = merchant;
     }
-
 }
+
+
 
 
--- webapp/Csye6225Webapp/src/main/java/csye6225Web/repositories/UserRepository.java
+++ webapp/Csye6225Webapp/src/main/java/csye6225Web/repositories/UserRepository.java
@@ -4,9 +4,7 @@ import csye6225Web.models.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * User repository for CRUD operations.
- */
+
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 }
